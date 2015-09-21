618132f3bdc9ea5ab260698318fc3f9aaf615898
antonio <me.verni@gmail.com>
2015-09-21 08:05:15 +0200

    Added mongo support through KeyStore abstaction. New Static keystore

--- restgate.go
+++ restgate.go
@@ -16,66 +16,219 @@ Thanks to Jeremy Saenz & Brendon Murphy for timing-attack protection
 */
 
 import (
-	// "errors"
 	"crypto/subtle"
 	"database/sql"
+	"errors"
 	"fmt"
+	e "github.com/pjebs/jsonerror"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"gopkg.in/unrolled/render.v1"
 	"log"
 	"net/http"
 	"strings"
-
-	e "github.com/pjebs/jsonerror"
-	"gopkg.in/unrolled/render.v1"
 )
 
-type AuthenticationSource int
+type KeyStore interface {
+	AddKey(key string, secret string) error
+	MatchKey(key string, secret string) (result bool, err error)
+}
 
-const (
-	Static   AuthenticationSource = 0
-	Database                      = 1
-)
+type keyStoreEntry struct {
+	index  int
+	secret string
+}
 
-//When AuthenticationSource=Static, Key(s)=Actual Key and Secret(s)=Actual Secret.
-//When AuthenticationSource=Database, Key[0]=Key_Column and Secret[0]=Secret_Column.
+type StaticKeyStore struct {
+	keyStore map[string]keyStoreEntry
+	keys     []string
+}
+
+func NewStaticKeyStore(storeSize int) *StaticKeyStore {
+	return &StaticKeyStore{keyStore: make(map[string]keyStoreEntry), keys: make([]string, 0, storeSize)}
+}
+
+func NewStaticKeyStoreFromKeys(keyMap map[string]string) (*StaticKeyStore, error) {
+	if keyMap == nil || len(keyMap) == 0 {
+		return nil, errors.New("Invalid key set")
+	}
+	ks := NewStaticKeyStore(len(keyMap))
+	for key, secret := range keyMap {
+		ks.AddKey(key, secret)
+	}
+	return ks, nil
+}
+
+func (ks *StaticKeyStore) AddKey(key string, secret string) error {
+	if ks.keyStore == nil || ks.keys == nil {
+		return errors.New("StaticKeyStore not initialized")
+	}
+	if key == "" || len(key) == 0 {
+		return errors.New("Cannot add empty key")
+	}
+	ks.keys = append(ks.keys, key)
+	ks.keyStore[key] = keyStoreEntry{index: len(ks.keyStore), secret: secret}
+	return nil
+}
+
+func (ks *StaticKeyStore) MatchKey(key string, secret string) (authenticationPassed bool, err error) {
+	authenticationPassed = false
+	err = nil
+	if keyEntry, ok := ks.keyStore[key]; ok {
+		if secureCompare(key, ks.keys[keyEntry.index]) { //Key matches
+			if (keyEntry.secret == "" && secret == "") || secureCompare(secret, keyEntry.secret) {
+				authenticationPassed = true
+			}
+		}
+	}
+	return authenticationPassed, err
+}
+
+type SQLKeyStore struct {
+	DB           *sql.DB
+	database     string
+	tableName    string
+	keyColumn    string
+	secretColumn string
+	postgres     bool
+}
+
+func NewSQLKeystore(db *sql.DB, database string, tableName string, keyColumn string, secretColumn string, postgres bool) (*SQLKeyStore, error) {
+	if database == "" {
+		return nil, errors.New("Database name cannot be empty")
+	}
+	if tableName == "" {
+		return nil, errors.New("Table name cannot be empty")
+	}
+	if keyColumn == "" {
+		return nil, errors.New("Key column name cannot be empty")
+	}
+	return &SQLKeyStore{DB: db, database: database, tableName: tableName, keyColumn: keyColumn, secretColumn: secretColumn, postgres: postgres}, nil
+}
+
+func (ks *SQLKeyStore) AddKey(key string, secret string) error {
+	secretDoesntExists := len(ks.secretColumn) == 0 || ks.secretColumn == ""
+	var preparedStatement string
+	if secretDoesntExists {
+		preparedStatement = fmt.Sprintf("INSERT INTO `%v` (%v) VALUES (?)", ks.tableName, ks.keyColumn)
+	} else {
+		preparedStatement = fmt.Sprintf("INSERT INTO `%v` (%v, %v) VALUES (?, ?)", ks.tableName, ks.keyColumn, ks.secretColumn)
+	}
+	stmt, err := ks.DB.Prepare(preparedStatement)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	if secretDoesntExists {
+		_, err = stmt.Exec(key)
+	} else {
+		_, err = stmt.Exec(key, secret)
+	}
+	return err
+}
+
+func (ks *SQLKeyStore) MatchKey(key string, secret string) (result bool, err error) {
+	secretDoesntExists := len(ks.secretColumn) == 0 || ks.secretColumn == ""
+	var preparedStatement string
+	if secretDoesntExists {
+		if ks.postgres == false { //COUNT(*) is definately faster on MYISAM and possibly InnoDB (MySQL engines)
+			preparedStatement = fmt.Sprintf("SELECT COUNT(1) FROM `%v` WHERE `%v`=?", ks.tableName, ks.keyColumn)
+		} else {
+			preparedStatement = fmt.Sprintf("SELECT COUNT(%v) FROM %v WHERE %v=$1", key, ks.tableName, ks.keyColumn)
+		}
+	} else {
+		if ks.postgres == false {
+			preparedStatement = fmt.Sprintf("SELECT COUNT(*) FROM `%v` WHERE `%v`=? AND `%v`=?", ks.tableName, ks.keyColumn, ks.secretColumn)
+		} else {
+			preparedStatement = fmt.Sprintf("SELECT COUNT(%v) FROM %v WHERE %v=$1 AND %v=$2", key, ks.tableName, ks.keyColumn, ks.secretColumn)
+		}
+	}
+	stmt, err := ks.DB.Prepare(preparedStatement)
+	if err != nil {
+		return false, err
+	}
+	defer stmt.Close()
+	var count int //stores query result
+	if secretDoesntExists {
+		err = stmt.QueryRow(key).Scan(&count)
+	} else {
+		err = stmt.QueryRow(key, secret).Scan(&count)
+	}
+
+	// log.Printf("result error: %+v", err)
+	// log.Printf("count: %+v", count)
+
+	if err == nil && count == 1 { // key found
+		return true, nil
+	}
+	return false, nil
+}
+
+type MongoKeyStore struct {
+	Session     *mgo.Session
+	database    string
+	collection  string
+	keyField    string
+	secretField string
+}
+
+func NewMongoKeystore(session *mgo.Session, database string, collection string, keyField string, secretField string) (*MongoKeyStore, error) {
+	if database == "" {
+		return nil, errors.New("Database name cannot be empty")
+	}
+	if collection == "" {
+		return nil, errors.New("Collection name cannot be empty")
+	}
+	if keyField == "" {
+		return nil, errors.New("Key field name cannot be empty")
+	}
+	return &MongoKeyStore{Session: session, database: database, collection: collection, keyField: keyField, secretField: secretField}, nil
+}
+
+func (ks *MongoKeyStore) AddKey(key string, secret string) error {
+	session := ks.Session.Clone()
+	defer session.Close()
+	db := session.DB(ks.database)
+	collection := db.C(ks.collection)
+	err := collection.Insert(bson.M{ks.keyField: key, ks.secretField: secret})
+	return err
+}
+
+func (ks *MongoKeyStore) MatchKey(key string, secret string) (result bool, err error) {
+	session := ks.Session.Clone()
+	defer session.Close()
+	db := session.DB(ks.database)
+	collection := db.C(ks.collection)
+	criteria := bson.M{
+		ks.keyField: key,
+	}
+	if secret != "" {
+		criteria[ks.secretField] = secret
+	}
+	count, err := collection.Find(criteria).Count()
+	if err != nil {
+		return false, err
+	}
+	return count == 1, nil
+}
+
 type Config struct {
-	*sql.DB
-	Key                []string
-	Secret             []string //Can be "" but not recommended
-	TableName          string
 	ErrorMessages      map[int]map[string]string
 	Context            func(r *http.Request, authenticatedKey string)
 	Debug              bool
-	Postgres           bool
 	HTTPSProtectionOff bool //Default is HTTPS Protection On
 }
 
 type RESTGate struct {
 	headerKeyLabel    string
 	headerSecretLabel string
-	source            AuthenticationSource
 	config            Config
+	store             KeyStore
 }
 
-func New(headerKeyLabel string, headerSecretLabel string, as AuthenticationSource, config Config) *RESTGate {
-	t := &RESTGate{headerKeyLabel: headerKeyLabel, headerSecretLabel: headerSecretLabel, source: as, config: config}
+func New(headerKeyLabel string, headerSecretLabel string, store KeyStore, config Config) *RESTGate {
+	t := &RESTGate{headerKeyLabel: headerKeyLabel, headerSecretLabel: headerSecretLabel, config: config, store: store}
 	log.Printf("RestGate initializing")
-
-	numberKeys := len(t.config.Key)
-	numberSecrets := len(t.config.Secret)
-
-	if numberKeys == 0 { //Key is not set
-		if t.config.Debug == true {
-			log.Printf("RestGate: Key is not set")
-		}
-		return nil
-	}
-
-	if numberSecrets > numberKeys { //Too many Secret's defined
-		if t.config.Debug == true {
-			log.Printf("RestGate: Too many Secrets defined. At most there should be 1 secret per key")
-		}
-		return nil
-	}
 
 	if headerKeyLabel == "" { //headerKeyLabel must be defined
 		if t.config.Debug == true {
@@ -116,34 +269,6 @@ func New(headerKeyLabel string, headerSecretLabel string, as AuthenticationSourc
 		log.Printf("\x1b[31mWARNING: HTTPS Protection is off. This is potentially insecure!\x1b[39;49m")
 	}
 
-	if as == Database {
-
-		if numberKeys != 1 { //We need exactly 1 Key (it represents field name in database)
-			if t.config.Debug == true {
-				log.Printf("RestGate: For Database mode, we need exactly 1 Key which represents the field name in the database table")
-			}
-			return nil
-		}
-
-		//Check if database is set.
-		//The developer should ensure a database has been selected (i.e. to prevent "No Database selected" error)
-		if t.config.DB == nil { //DB is not set
-			if t.config.Debug == true {
-				log.Printf("RestGate: Database is not set. Be sure that a database name is selected")
-			}
-			return nil
-		}
-
-		//Check if table is set
-		if t.config.TableName == "" { //Table name is not set
-			if t.config.Debug == true {
-				log.Printf("RestGate: For Database mode, a table name is required")
-			}
-			return nil
-		}
-
-	}
-
 	return t
 }
 
@@ -170,114 +295,24 @@ func (self *RESTGate) ServeHTTP(w http.ResponseWriter, req *http.Request, next h
 		return
 	}
 
-	if self.source == Static {
-
-		secretDoesntExist := len(self.config.Secret) == 0
-		authenticationPassed := false
-
-		//First search through all keys
-		for index, element := range self.config.Key {
-			if secureCompare(key, element) { //Key matches
-
-				//Now check if secret matches
-				if secretDoesntExist {
-					//Authentication PASSED
-					authenticationPassed = true
-					break
-				} else if index > (len(self.config.Secret) - 1) { //Out of Range so corresponding secret doesn't exist
-					//Authentication PASSED
-					authenticationPassed = true
-					break
-				} else {
-					//Corresponding Secret exists
-					if secureCompare(secret, self.config.Secret[index]) {
-						//Authentication PASSED
-						authenticationPassed = true
-						break
-					} else {
-						//Authentication FAILED
-						authenticationPassed = false
-						break
-					}
-				}
-			}
+	authenticationPassed, err := self.store.MatchKey(key, secret)
+	if err != nil {
+		if self.config.Debug == true {
+			log.Printf("RestGate: Run time error: %+v", err)
 		}
-
-		//Authentication FAILED - No Key's matched
-		if authenticationPassed == false {
-			r := render.New(render.Options{})
-			r.JSON(w, http.StatusUnauthorized, self.config.ErrorMessages[2]) //"Unauthorized Access"
-			return
-		} else { //Authentication PASSED
-			if self.config.Context != nil {
-				self.config.Context(req, key)
-			}
-			next(w, req)
-		}
-
-	} else if self.source == Database {
-		db := self.config.DB
-
-		secretDoesntExists := len(self.config.Secret) == 0 || self.config.Secret[0] == ""
-
-		var preparedStatement string
-		if secretDoesntExists {
-			if self.config.Postgres == false { //COUNT(*) is definately faster on MYISAM and possibly InnoDB (MySQL engines)
-				preparedStatement = fmt.Sprintf("SELECT COUNT(*) FROM `%v` WHERE `%v`=?", self.config.TableName, self.config.Key[0])
-			} else {
-				preparedStatement = fmt.Sprintf("SELECT COUNT(%v) FROM %v WHERE %v=$1", self.config.Key[0], self.config.TableName, self.config.Key[0])
-			}
-		} else {
-			if self.config.Postgres == false {
-				preparedStatement = fmt.Sprintf("SELECT COUNT(*) FROM `%v` WHERE `%v`=? AND `%v`=?", self.config.TableName, self.config.Key[0], self.config.Secret[0])
-			} else {
-				preparedStatement = fmt.Sprintf("SELECT COUNT(%v) FROM %v WHERE %v=$1 AND %v=$2", self.config.Key[0], self.config.TableName, self.config.Key[0], self.config.Secret[0])
-			}
-		}
-
-		stmt, err := db.Prepare(preparedStatement)
-		if err != nil {
-			if self.config.Debug == true {
-				log.Printf("RestGate: Run time database error: %+v", err)
-			}
-			r := render.New(render.Options{})
-			r.JSON(w, http.StatusUnauthorized, self.config.ErrorMessages[99]) //"Software Developers have not setup authentication correctly"
-			return
-		}
-		defer stmt.Close()
-
-		var count int //stores query result
-		if secretDoesntExists {
-			err = stmt.QueryRow(key).Scan(&count)
-		} else {
-			err = stmt.QueryRow(key, secret).Scan(&count)
-		}
-
-		// log.Printf("result error: %+v", err)
-		// log.Printf("count: %+v", count)
-
-		if err == nil && count == 1 {
-			//Authentication PASSED
-			if self.config.Context != nil {
-				self.config.Context(req, key)
-			}
-			next(w, req)
-		} else { //==sql.ErrNoRows or count == 0
-			//Something went wrong
-			if self.config.Debug == true && count > 1 {
-				log.Printf("RestGate: Database query returned more than 1 identical Key. Make sure the KEY field in the table is set to UNIQUE")
-			}
-			r := render.New(render.Options{})
-			r.JSON(w, http.StatusUnauthorized, self.config.ErrorMessages[2]) //"Unauthorized Access"
-			return
-		}
-
-	} else {
 		r := render.New(render.Options{})
-		r.JSON(w, http.StatusUnauthorized, self.config.ErrorMessages[99]) //"Software Developers have not setup authentication correctly"
-		return
+		jerr := e.New(2, err.Error(), "", "com.github.pjebs.restgate").Render()
+		r.JSON(w, http.StatusUnauthorized, jerr) //"Unauthorized Access"
 	}
-
+	if !authenticationPassed {
+		r := render.New(render.Options{})
+		r.JSON(w, http.StatusUnauthorized, self.config.ErrorMessages[2]) //"Unauthorized Access"
+	} else {
+		if self.config.Context != nil {
+			self.config.Context(req, key)
+		}
+		next(w, req)
+	}
 }
 
 // secureCompare performs a constant time compare of two strings to limit timing attacks.
